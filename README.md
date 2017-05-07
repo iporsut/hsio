f840fcdb6ef728171462c268669f3afad772cd58
Weerasak Chongnguluam <singpor@gmail.com>
2017-05-07 16:30:59 +0700

    Learn Haskell IO by calculate Age from birthday

---
+++ README.md
@@ -0,0 +1,2 @@
+# hsio
+# Learn Haskell I/O
---
+++ app/Main.hs
@@ -0,0 +1,25 @@
+module Main where
+
+import Data.Time
+import Data.List.Split
+
+main :: IO ()
+main = do
+  putStrLn "Hello, what is your name?"
+  name <- getLine
+  putStrLn ("Hello, " ++ name ++ "!")
+
+  putStrLn "Birthday (format: dd/mm/yyyy, e.g.: 21/06/1985)"
+  birthdayStr <- getLine
+  birthday <- (parseTimeM
+               False
+               defaultTimeLocale
+               "%d/%m/%Y"
+               birthdayStr :: IO UTCTime)
+  let (by, bm, bd) = toGregorian (utctDay birthday)
+
+  nowUTC <- getCurrentTime
+  let (ny, nm, nd) = toGregorian (utctDay nowUTC)
+
+  let age = if (nm >= bm) && (nd >= bd) then (ny - by) else (ny - by - 1)
+  putStrLn ("You are " ++ (show age) ++ " years old.")
