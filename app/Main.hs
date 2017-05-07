272767e199d4308dc6f1592c48984e6602854297
Weerasak Chongnguluam <singpor@gmail.com>
2017-05-07 16:46:04 +0700

    Comment describe function and scope import

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,6 @@
 module Main where
 
-import Data.Time
+import Data.Time (defaultTimeLocale, getCurrentTime, parseTimeM, toGregorian, utctDay, UTCTime)
 
 main :: IO ()
 main = do
@@ -11,14 +11,19 @@ main = do
   putStrLn "Birthday (format: dd/mm/yyyy, e.g.: 21/06/1985)"
   birthdayStr <- getLine
   birthday <- (parseTimeM
-               False
+               False                        -- Don't accept leading and trailing whitespace.
                defaultTimeLocale
-               "%d/%m/%Y"
-               birthdayStr :: IO UTCTime)
-  let (by, bm, bd) = toGregorian (utctDay birthday)
+               "%d/%m/%Y"                   -- Format: dd/mm/yyyy
+               birthdayStr :: IO UTCTime)   -- Result in IO context
+  let (by, bm, bd) = toGregorian (utctDay birthday)  -- Get (year, month, day)
 
   nowUTC <- getCurrentTime
-  let (ny, nm, nd) = toGregorian (utctDay nowUTC)
+  let (ny, nm, nd) = toGregorian (utctDay nowUTC)    -- Get (year, month, day)
 
-  let age = if (nm >= bm) && (nd >= bd) then (ny - by) else (ny - by - 1)
+  let age = if (nm >= bm) && (nd >= bd)
+            then
+              (ny - by)      -- Pass birthday in current year.
+            else
+              (ny - by - 1)  -- Don't pass birthday in current year.
+
   putStrLn ("You are " ++ (show age) ++ " years old.")
